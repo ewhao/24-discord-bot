9c2b0e2ddcf7281e65df39cbd3f15062940b4603
ewhao <emilyhao.09@gmail.com>
2025-07-22 12:01:21 -0400

    format rules

--- commands/game/rules.js
+++ commands/game/rules.js
@@ -7,14 +7,16 @@ module.exports = {
 	async execute(interaction) {
         const rules = "The 24 puzzle is an arithmetical puzzle in which the objective " +
             "is to find a way to manipulate four integers 1-13 so that the end result is 24." +
-            "For example, for the numbers [4, 7, 8, 8], a possible solution is **(7-(8/8))*4**.\n\n" +
+            "For example, for the numbers `4, 7, 8, 8`, a possible solution is `(7-(8/8))*4`.\n\n" +
 
-            "Each number of four numbers provided in a set must be used exactly once, " + 
-            "and accepted operations are addition (+), subtraction (-), multiplication (-), " +
-            "division(/), and parenthesis(()).\n\n" +
+            "The rules for this version are as follows;\n" + 
+            "- Each number of four numbers provided in a set must be used exactly once\n" + 
+            "- Accepted operations are addition (+), subtraction (-), multiplication (-), " +
+            "division(/), and parenthesis(()).\n" +
+            "- Answers should be wrapped in backticks (`) to prevent other markdown from being applied.\n\n" +
 
-            "See [wikipedia](https://en.wikipedia.org/wiki/24_(puzzle)) to learn more about " +
-            "the game and variations";
+            "See [wikipedia](<https://en.wikipedia.org/wiki/24_(puzzle)) to learn more about " +
+            "the game and variations.";
 		await interaction.reply(rules);
 	},
 };
